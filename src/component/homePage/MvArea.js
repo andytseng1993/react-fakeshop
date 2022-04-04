ed7212101de56bcc4985c7e55a900ec7f6018110
Andy Tseng <anby1611@gmail.com>
2022-04-04 00:25:05 +0000

    finish fade keyframes

--- src/component/homePage/MvArea.js
+++ src/component/homePage/MvArea.js
@@ -3,21 +3,22 @@ import classes from './MvArea.module.css'
 
 function MvArea(){
     const [scroll,setScroll] = useState(false)
-
+    const scrollClass = scroll? classes.scroll:''
     const scrollCheck = ()=>{ 
         if(window.scrollY>0) return setScroll(true)
         else return setScroll(false)
     }
+
     useEffect(()=>{
-        window.removeEventListener('scroll',scrollCheck)
+        
         window.addEventListener('scroll',scrollCheck)
         return ()=>window.removeEventListener('scroll',scrollCheck)
-    },[scroll])
+    },[])
     return (
-        <div className={classes.mvArea}>
+        <div className={`${classes.mvArea} ${scrollClass}`}>
             <ul className={classes.mvList}>
-                <li></li>
-                <li></li>
+                <li className={classes.image1}></li>
+                <li className={classes.image2}></li>
             </ul>    
         </div>
     )
