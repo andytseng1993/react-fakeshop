ed1398643d094b2c0ec11a15ed339eb557980c29
Andy Tseng <anby1611@gmail.com>
2022-04-05 06:43:06 +0000

    products loading

--- src/component/shopPage/ProductListing.js
+++ src/component/shopPage/ProductListing.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { setProducts } from "../../redux/actions";
 import ProductComponent from "./ProductComponent";
@@ -9,8 +9,10 @@ import ProductComponent from "./ProductComponent";
 const ProductListing=()=>{
     const products = useSelector((state)=>state.allProducts.products)
     const dispatch = useDispatch()
+    const [isLoading,setIsLoading] = useState(true)
 
     useEffect(()=>{
+        setIsLoading(true)
         const axiosData = ()=>{
                 return axios.get('https://fakestoreapi.com/products')
              .then((res) => {
@@ -20,12 +22,22 @@ const ProductListing=()=>{
         }
         axiosData().then((data)=>{
             dispatch(setProducts(data))
+            setIsLoading(false)
         })
       },[])
       console.log(products);
+
+     if(isLoading){
+        return (
+            <div className='productList'>
+                 <h3>Loading...</h3>
+            </div>
+        )
+     }
     return (
-        <div>
+        <div className='productList'>
              <ProductComponent/>
+             <span className="wrap" />
         </div>
     )
 }
