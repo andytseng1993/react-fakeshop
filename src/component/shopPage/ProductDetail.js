0327718c63ccd22db2b19a982ce6c42d5423959d
Andy Tseng <anby1611@gmail.com>
2022-04-05 20:55:55 +0000

    productDetail template

--- src/component/shopPage/ProductDetail.js
+++ src/component/shopPage/ProductDetail.js
@@ -1,20 +1,23 @@
+import axios from "axios"
 import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import { removeProduct, selectProduct } from "../../redux/actions"
-import { useAxios } from "./ProductListing"
+import classes from './ProductDetail.module.css'
 
 
 const ProductDetail=()=>{
     const params = useParams()
     const [isLoading,setIsLoading] = useState(true)
-    const data = useAxios(`https://fakestoreapi.com/products/${params.productId}`)
     const dispatch= useDispatch()
     const productDetail = useSelector((state)=> state.productDetail)
-
+    const {image, title,price,description,category} = productDetail
+    console.log(productDetail)
+    
     useEffect(()=>{
         setIsLoading(true)
-        data.then(data => {
+        axios.get(`https://fakestoreapi.com/products/${params.productId}`)
+        .then(({data}) => {
             setIsLoading(false)
             dispatch(selectProduct(data))
         })
@@ -26,18 +29,41 @@ const ProductDetail=()=>{
     if(isLoading){
         return (
             <>
-                <h2>Loading....</h2>
+                <h2 style={{marginTop:'30px'}}>Loading....</h2>
+            </>
+        )
+    }
+    if(Object.keys(productDetail).length===0){
+        return (
+            <>
+                <h2 style={{marginTop:'30px'}}>Sorry! No any product here....</h2>
             </>
         )
     }
     return (
-        <div>
-            <div>
-                <div>
-                    {productDetail.title}
+        <section className={classes.productDetail}>
+            <div className={classes.image}>
+                <img src={image} alt={title}></img>
+            </div>
+            <div className={classes.content}>
+                <div className={classes.title}>
+                    {title}
+                </div>
+                <div className={classes.price}>
+                    ${price}
+                </div>
+                <div className={classes.category}>
+                    {category}
+                </div>
+                <div className={classes.description}>
+                    {description}
+                </div>
+                <div className={classes.cart}>
+                    Add to Cart
                 </div>
             </div>
-        </div>
+        </section>
+        
     )
 }
 export default ProductDetail
--- src/component/shopPage/ProductListing.js
+++ src/component/shopPage/ProductListing.js
@@ -4,23 +4,16 @@ import { useDispatch, useSelector } from "react-redux"
 import { setProducts } from "../../redux/actions";
 import ProductComponent from "./ProductComponent";
 
-export const useAxios = (url)=>{
-    return axios.get(url)
-    .then((res) => {
-        return res.data
-    })
-    .catch(err=>console.error(err))
-}
-
 const ProductListing=()=>{
     const products = useSelector((state)=>state.allProducts.products)
     const dispatch = useDispatch()
     const [isLoading,setIsLoading] = useState(true)
-    const data = useAxios('https://fakestoreapi.com/products')
+    
     
     useEffect(()=>{
         setIsLoading(true)
-        data.then((data)=>{
+        axios.get('https://fakestoreapi.com/products')
+        .then(({data})=>{
             dispatch(setProducts(data))
             setIsLoading(false)
         })
