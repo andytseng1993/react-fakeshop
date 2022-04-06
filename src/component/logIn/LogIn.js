348740901870d9d726ad3378f50a26b451fe91f8
Andy Tseng <anby1611@gmail.com>
2022-04-06 22:30:11 +0000

    password hideHandler

--- src/component/logIn/LogIn.js
+++ src/component/logIn/LogIn.js
@@ -1,13 +1,19 @@
 import classes from './LogIn.module.css'
-import { useRef } from "react";
-
+import { useRef, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faEyeSlash,faEye} from "@fortawesome/free-solid-svg-icons";
 
 const LogIn=(props)=>{
     const emailRef = useRef('')
     const passwordRef = useRef('')
+    const [hide,setHide] = useState(true)
+    
     const submitHandler=(event)=>{
         event.preventDefault()
         console.log(emailRef.current.value,passwordRef.current.value);
+    }
+    const hideHandler=()=>{
+        setHide(!hide)
     }
     return(
         <section className={classes.login}>
@@ -16,7 +22,15 @@ const LogIn=(props)=>{
                 <div className={classes.closeBtn}>X</div>
                 <form onSubmit={submitHandler}>
                     <input type='email' required id='email' ref={emailRef} placeholder='Email Adress'></input>
-                    <input type='password' required id='password' ref={passwordRef} placeholder='Password'></input>
+                    <div className={classes.password}>
+                        <input type={hide?'password':'text'} required id='password' ref={passwordRef} placeholder='Password'></input>
+                        <div className={classes.hide} onClick={hideHandler}>
+                            {
+                                hide?<FontAwesomeIcon icon={faEye}/>: <FontAwesomeIcon icon={faEyeSlash} /> 
+                            }
+                        </div>
+                    </div>
+
                     <button className={classes.submit}>Log In</button>
                 </form>
             </div>
