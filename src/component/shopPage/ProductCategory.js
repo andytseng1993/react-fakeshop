260147c3a412f2bf657ffebb435972b7a6d69fa9
Andy Tseng <anby1611@gmail.com>
2022-04-06 01:30:46 +0000

    category button

--- src/component/shopPage/ProductCategory.js
+++ src/component/shopPage/ProductCategory.js
@@ -4,24 +4,33 @@ const ProductCategory=()=>{
     const dispatch=useDispatch()
     const select = useSelector((state)=> state.selectCategory)
     const products = useSelector((state)=> state.allProducts.products)
-    let map={}
-    let sort=[]
-    products.forEach(({category})=>{
-        if(!map[category]){
-            map[category] = 1
-            sort.push(category)
-            sort.sort()
-        }
-    })
-    console.log(sort);
+
+    function allCategories (products){
+       let map={}
+        let sort=[]
+        products.forEach(({category})=>{
+            if(!map[category]){
+                map[category] = 1
+                sort.push(category)
+                sort.sort()
+            }
+        })
+        return sort 
+    }
+    const categoryArray = allCategories(products)
+
     return(
         <>
-            <select name="cars" id="cars" >
-                <option value="All Products">All Products</option>
-                <option value="saab">Saab</option>
-                <option value="opel">Opel</option>
-                <option value="audi">Audi</option>
-            </select>
+            {
+                categoryArray.map((category)=>(
+                    <button 
+                        key={category} 
+                        onClick={()=> console.log(category)}
+                    >
+                        {category}
+                    </button>
+                ))
+            }
         </>
     )
 }
